fa72dec60a974e4cc01d7f7d7dd5c112f6800c06
moahmed salah taman <moahmed.salah.taman@gmail.com>
2021-02-07 00:52:21 +0200

    Esp_init Function

--- HAL/WIFI/wifi.c
+++ HAL/WIFI/wifi.c
@@ -20,4 +20,60 @@ void WIFI_sendPacket(INT8U* packet,INT8U packetLen){
 	vTaskDelay(100*TICKS_IN_MILLISECOND);
 	Uart_Putchars(WIFI_UART,packet,packetLen);
 }
+void ESP_Init (char *SSID, char *PASSWD)
+{
+	char data[80];
 
+	Ringbuf_init();
+
+	Uart_sendstring("AT+RST\r\n", wifi_uart);
+	Uart_sendstring("RESETTING.", pc_uart);
+	for (int i=0; i<5; i++)
+	{
+		Uart_sendstring(".", pc_uart);
+		HAL_Delay(1000);
+	}
+
+	/********* AT **********/
+	Uart_sendstring("AT\r\n", wifi_uart);
+	while(!(Wait_for("AT\r\r\n\r\nOK\r\n", wifi_uart)));
+	Uart_sendstring("AT---->OK\n\n", pc_uart);
+
+
+	/********* AT+CWMODE=1 **********/
+	Uart_sendstring("AT+CWMODE=1\r\n", wifi_uart);
+	while (!(Wait_for("AT+CWMODE=1\r\r\n\r\nOK\r\n", wifi_uart)));
+	Uart_sendstring("CW MODE---->1\n\n", pc_uart);
+
+
+	/********* AT+CWJAP="SSID","PASSWD" **********/
+	Uart_sendstring("connecting... to the provided AP\n", pc_uart);
+	sprintf (data, "AT+CWJAP=\"%s\",\"%s\"\r\n", SSID, PASSWD);
+	Uart_sendstring(data, wifi_uart);
+	while (!(Wait_for("WIFI GOT IP\r\n\r\nOK\r\n", wifi_uart)));
+	sprintf (data, "Connected to,\"%s\"\n\n", SSID);
+	Uart_sendstring(data,pc_uart);
+
+
+	/********* AT+CIFSR **********/
+	Uart_sendstring("AT+CIFSR\r\n", wifi_uart);
+	while (!(Wait_for("CIFSR:STAIP,\"", wifi_uart)));
+	while (!(Copy_upto("\"",buffer, wifi_uart)));
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	int len = strlen (buffer);
+	buffer[len-1] = '\0';
+	sprintf (data, "IP ADDR: %s\n\n", buffer);
+	Uart_sendstring(data, pc_uart);
+
+
+	Uart_sendstring("AT+CIPMUX=1\r\n", wifi_uart);
+	while (!(Wait_for("AT+CIPMUX=1\r\r\n\r\nOK\r\n", wifi_uart)));
+	Uart_sendstring("CIPMUX---->OK\n\n", pc_uart);
+
+	Uart_sendstring("AT+CIPSERVER=1,80\r\n", wifi_uart);
+	while (!(Wait_for("OK\r\n", wifi_uart)));
+	Uart_sendstring("CIPSERVER---->OK\n\n", pc_uart);
+
+	Uart_sendstring("Now Connect to the IP ADRESS\n\n", pc_uart);
+
+}
